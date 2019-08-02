acfa65332786aaa362a2d9404525e25789bcfe52
Zaephor <Zaephor@users.noreply.github.com>
2019-08-01 20:05:09 -0700

    test: changed __tests__ folder to test

--- test/ah-objection-plugin.test.js
+++ test/ah-objection-plugin.test.js
@@ -1,26 +1,27 @@
 /* eslint-env node, mocha */
+/* eslint-disable no-unused-expressions */
 const path = require('path')
-const {expect} = require('chai')
+const { expect } = require('chai')
 const ActionHero = require('actionhero')
 const actionhero = new ActionHero.Process()
 
 process.env.PROJECT_ROOT = path.join(require.resolve('actionhero'), '..')
-let knexConfig = require(path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..', 'config', 'ah-knex-plugin.js'))
-let knexEnv = (process.env.NODE_ENV && knexConfig[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
+const knexConfig = require(path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..', 'config', 'ah-knex-plugin.js'))
+const knexEnv = (process.env.NODE_ENV && knexConfig[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
 let api
 
 describe('ah-objection-plugin', () => {
-  let configChanges = {
+  const configChanges = {
     'ah-knex-plugin': knexConfig[knexEnv]['ah-knex-plugin'](ActionHero.api),
     // 'ah-objection-plugin': config[environment]['ah-objection-plugin'](ActionHero.api),
     plugins: {
-      'ah-knex-plugin': {path: path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..')},
-      'ah-objection-plugin': {path: path.join(__dirname, '..')}
+      'ah-knex-plugin': { path: path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..') },
+      'ah-objection-plugin': { path: path.join(__dirname, '..') }
     }
   }
 
   before(async () => {
-    api = await actionhero.start({configChanges})
+    api = await actionhero.start({ configChanges })
   })
 
   after(async () => { await actionhero.stop() })
